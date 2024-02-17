4a494ad696cb97b7907475186d0648210f73b338
nikhil-pillare <nikhilpillare98@gmail.com>
2024-02-17 10:46:51 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,13 @@ const userRouter = require('./routes/user.route')
 require("dotenv").config();
 var cors = require('cors')
 const app = express();
-
-app.use(express.json());
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, 
+    optionsSuccessStatus: 204,
+  };
+app.use(express.json(corsOptions));
 app.use(cors())
 
 app.use("/user", userRouter)
@@ -15,7 +20,7 @@ app.use('/movies', movieRouter);
 app.listen(process.env.PORT, async() => {
         try {
             mongoose.connect(process.env.MONGOURL)
-            console.log('server started and database connected'); 
+            console.log('server started and database connect'); 
         } catch (error) {
            console.log(error); 
         }
