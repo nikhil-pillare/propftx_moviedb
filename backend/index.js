76dc86649e9c845550c012fe3af94631b5b56f8c
nikhil-pillare <nikhilpillare98@gmail.com>
2024-02-17 13:11:07 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: 'https://propftx-moviedb.vercel.app',
+    origin: ['https://propftx-moviedb.vercel.app','http://localhost:3000']
 }))
 
 app.use("/user", userRouter)
